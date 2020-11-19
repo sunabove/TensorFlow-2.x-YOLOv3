c180e235a24e75f6576cc855f0dceb72655eb587
openvisual <sunabove@gmail.com>
2020-11-18 23:40:53 -1000

    **

--- WebCamera.py
+++ WebCamera.py
@@ -6,6 +6,7 @@ import cv2
 import numpy as np 
 import threading
 import socketserver
+from time import sleep
 from threading import Condition
 from http import server
 
@@ -31,19 +32,24 @@ class StreamingOutput(object):
     pass
 
     def write(self, buf):
-        if buf.startswith(b'\xff\xd8'):
-            # New frame, copy the existing buffer's content and notify all
-            # clients it's available
-            self.buffer.truncate()
-            with self.condition:
-                self.frame = self.buffer.getvalue()
-                self.condition.notify_all()
-            self.buffer.seek(0)
+        # New frame, copy the existing buffer's content and notify all
+        # clients it's available
+
+        self.buffer.truncate()
+        
+        with self.condition:
+            self.frame = self.buffer.getvalue()
+            self.condition.notify_all()
+        pass
+
+        self.buffer.seek(0) 
+            
         return self.buffer.write(buf)
     pass
 pass
 
 class StreamingHandler(server.BaseHTTPRequestHandler):
+    
     def do_GET(self):
         if self.path == '/':
             self.send_response(301)
@@ -64,18 +70,18 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
             self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
             self.end_headers()
             try:
+                output = camera.output
                 while True:
                     with output.condition:
                         output.condition.wait()
                         frame = output.frame
-                    pass
-
                     self.wfile.write(b'--FRAME\r\n')
                     self.send_header('Content-Type', 'image/jpeg')
                     self.send_header('Content-Length', len(frame))
                     self.end_headers()
                     self.wfile.write(frame)
                     self.wfile.write(b'\r\n')
+                pass
             except Exception as e:
                 logging.warning(
                     'Removed streaming client %s: %s',
@@ -92,38 +98,46 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     daemon_threads = True
 pass
 
-class Cv2Camera(object):
+class Camera(object):
 
-    def __init__(self, resolution='640x480', framerate=24):
-        self.video = cv2.VideoCapture(-1)
-        self.video.set(cv2.CAP_PROP_FPS, framerate)
-        self.video.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'X264'))
+    def __init__(self):
+        self.output = StreamingOutput()
         # 전체 프레임 카운트 
         self.frame_cnt = 0 
-    pass
-
-    def __enter__(self) :
-        pass
-    pass
-
-    def __exit__(self, exc_type, exc_value, tb):
-        self.video.release()
-    pass
+        
+        self.video = cv2.VideoCapture(-1)
+        self.video.set(cv2.CAP_PROP_FPS, 24)
+        self.video.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'X264'))
+    pass 
     
     def __del__(self):
         self.video.release()
     pass
 
-    def start_recording(self, output) :
+    def start_recording(self) :
         x = threading.Thread(target=self.start_recording_impl, args=[] )
+        x.start()
     pass
 
-    def start_recording_impl(self, output) :
-        img = self.get_image()
+    def start_recording_impl(self) :
+        self.recording = True
+
+        while self.recording: 
+            #print( "recording ...." , end="" )
+            image = self.get_image()
+
+            _, jpg = cv2.imencode('.jpg', image ) 
+            self.output.write( jpg )
+            #print( "Done." )
+        pass
+    pass
+
+    def stop_recording(self):
+        self.recording = False 
     pass
 
     def get_image(self):
-        success, img = self.video.read()
+        success, image = self.video.read()
 
         self.frame_cnt += 1 
 
@@ -140,9 +154,9 @@ class Cv2Camera(object):
         h = 20   # line height
 
         txt = f"Hello [{self.frame_cnt}]"
-        self.putTextLine( img, txt , x, y )
+        self.putTextLine( image, txt , x, y )
 
-        return img
+        return image
     pass
 
     def get_frame( self ) : 
@@ -155,7 +169,7 @@ class Cv2Camera(object):
         return jpg.tobytes()
     pass
 
-    def putTextLine(self, img, txt, x, y ) :
+    def putTextLine(self, image, txt, x, y ) :
         # opencv 이미지에 텍스트를 그린다.
         font = cv2.FONT_HERSHEY_SIMPLEX
         fs = 0.4  # font size(scale)
@@ -164,25 +178,24 @@ class Cv2Camera(object):
         bg_color = (255, 255, 255) # text background color
         fg_color = (255,   0,   0) # text foreground color
 
-        cv2.putText(img, txt, (x, y), font, fs, bg_color, ft + 2, cv2.LINE_AA)
-        cv2.putText(img, txt, (x, y), font, fs, fg_color, ft    , cv2.LINE_AA) 
+        cv2.putText(image, txt, (x, y), font, fs, bg_color, ft + 2, cv2.LINE_AA)
+        cv2.putText(image, txt, (x, y), font, fs, fg_color, ft    , cv2.LINE_AA) 
     pass
 pass
 
 if __name__=='__main__':
 
-    with Cv2Camera(resolution='640x480', framerate=24) as camera:
-        output = StreamingOutput()
-        #Uncomment the next line to change your Pi's Camera rotation (in degrees)
-        #camera.rotation = 90
-        camera.start_recording(output)
-        try:
-            address = ('', 80)
-            server = StreamingServer(address, StreamingHandler)
-            server.serve_forever()
-        finally:
-            camera.stop_recording()
-        pass
-    pass
+    camera = Camera()
+    #Uncomment the next line to change your Pi's Camera rotation (in degrees)
+    #camera.rotation = 90
+    camera.start_recording()
+    try:
+        address = ('', 80)
+        server = StreamingServer(address, StreamingHandler)
+        print( "Hello...\nServer is running now ....." )
+        server.serve_forever()
+    finally:
+        camera.stop_recording()
+    pass 
 
 pass
